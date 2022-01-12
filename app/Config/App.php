d227e00286ea571533306806f0b36ea10b59abb9
Alexander Torres <alexskate19@gmail.com>
2022-01-11 22:49:58 -0500

    Cambios Api db

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'https://cooperativa-pugacho.herokuapp.com';
 
     /**
      * --------------------------------------------------------------------------
